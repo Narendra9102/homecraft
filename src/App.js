d6ee7f6f3694c85d1ac5a6b1ae970297898077e9
Narendra <narendrasompalli02@gmail.com>
2024-09-15 11:17:47 +0530

    Updated App.js and added Login component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    if (!username || !password) {
+      setError('Username and password are required');
+      return;
+    }
+  
+    try {
+      const response = await fetch('http://localhost:8000/api/login/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || 'Login failed');
+      }
+  
+      const data = await response.json();
+      console.log('Login successful:', data);
+      localStorage.setItem('access_token', data.access);
+      localStorage.setItem('refresh_token', data.refresh);
+      navigate('/home');
+    } catch (error) {
+      console.error('Error during login:', error);
+      setError('Login failed');
+    }
+  };
+  
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>} 
+    </div>
+  );
+};
+
+export default Login;
